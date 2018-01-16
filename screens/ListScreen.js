7bcc54e2e58cecf9eb863e136171e56074fb08a2
actooalizame <gndalmedo@gmail.com>
2018-01-16 16:38:21 -0300

    working v 2.0

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -2,7 +2,7 @@ import Meteor, {createContainer} from 'react-native-meteor';
 import React from 'react';
 import MeteorListViewComponent from '../components/meteorListView';
 
-Meteor.connect('ws://192.168.1.84:3000/websocket');//do this only once
+Meteor.connect('ws://192.168.1.85:3000/websocket');//do this only once
 
 export default ListScreen = createContainer(props => {
   const itemsHandle = Meteor.subscribe('items');
